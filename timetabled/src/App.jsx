711a90fb90186862fcd42cba770eeb159141bb08
kaylathornton55@icloud.com <kayla_thornton@Kaylas-MacBook-Air.local>
2024-09-18 22:19:08 -0400

    Created Calendar component with blocks for different events

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,5 +1,6 @@
 // imported CSS file for styling
 import './App.css';
+import Calendar from './components/Calendar';
 
 // This is our root (parent to all components)
 const App = () => {
@@ -7,7 +8,8 @@ const App = () => {
   return (
     <div className="App">
       <h1>A week in Paradise 🌴</h1>
-      <h2>Come enjoy a week long vacation stay in Costa Rica</h2>
+      <h2>Come enjoy a week long vacation stay in Costa Rica!</h2>
+      <Calendar />
     </div>
   )
 }
