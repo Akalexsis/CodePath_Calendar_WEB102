6e193d0ca47797894468d48896c5f6196b9eb5d6
kaylathornton55@icloud.com <kayla_thornton@Kaylas-MacBook-Air.local>
2024-09-18 21:58:03 -0400

    Updated root component

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+// imported CSS file for styling
+import './App.css';
+
+// This is our root (parent to all components)
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>A week in Paradise 🌴</h1>
+      <h2>Come enjoy a week long vacation stay in Costa Rica</h2>
+    </div>
+  )
+}
+
+// exports App component to be used in other files
+export default App
